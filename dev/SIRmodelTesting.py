f417a4f00337c1d727a51e8a920c107002376611
George Kent-Scheller <georgeks@bu.edu>
2021-01-15 21:17:56 -0500

    did eulers method for sir testing in python

--- dev/SIRmodelTesting.py
+++ dev/SIRmodelTesting.py
@@ -1,9 +1,53 @@
 # test python file for testing sir model 
+def dy(y,t):
+	return (y*y+t)
+def EulerMethod(t,t0,y0,stepsize):
+	tlen = t-t0
+	y = y0
+	tcurr = t0
+	for i in range(int(float(tlen)/float(stepsize))):
+		y = y + stepsize*dy(y,tcurr) 
+		tcurr = tcurr+stepsize
+		print(tcurr,"|",y)
+	return y
+EulerMethod(5,0,0,0.5)
 
-a = 0.5
-b = 0.2 
-InitiallyInf = 1 
-Population = 100
+a = 2.0
+b = 1
+InitiallyInf =0.005
+Population = 1
 InitallySus = Population -InitiallyInf
 InitallyRec = 0 
-print(InitallySus)
+
+S = InitallySus
+I = InitiallyInf
+R = InitallyRec
+def dS(S,I):
+	#print("in ds:",a,"\t",S,"\t",I)
+	return -a*S*I
+def dI(S,I):
+	#print("in dI:",a,"\t",S,"\t",I)
+	return a*S*I -b*I	
+def dR(I):
+	#print("in dr:",a,"\t",S,"\t",I)
+	return b*I
+def EulerMethodSRI(S,R,I,t,t0,stepsize):
+	tlen = t-t0
+	tcurr = t0
+	sc = S
+	ic = I
+	rc = R
+	for i in range(int(float(tlen)/float(stepsize))):
+		Sn = sc + stepsize*dS(sc,ic) 
+		In = ic + stepsize*dI(sc,ic) 
+		Rn = rc + stepsize*dR(ic)
+
+
+		sc = Sn
+		ic = In
+		rc = Rn 
+		
+		tcurr = tcurr+stepsize
+		print(tcurr,"|",sc,"|",ic,"|",rc)
+	return sc,ic,rc
+EulerMethodSRI(S,R,I,20,0,0.1)		
