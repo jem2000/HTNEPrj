315de70992cd7942b4b60b0afaa0ed114f15f9c4
Justin Melville <56002757+jem2000@users.noreply.github.com>
2021-01-16 15:21:33 -0500

    Update SIR_Form.js

--- SIR_Form.js
+++ SIR_Form.js
@@ -8,75 +8,91 @@
 // b is the rate of recovery
 
 //global vars
-let t = 20;
-let t0 = 0.0;
-let ss = 0.1;
-let s0 = 0.995;
-let i0 = 0.005;
-let r0 = 0.0;
-let a = 2.0;
-let b = 1.0;
+let pop = 874961.0
+let t = 20
+let t0 = 0.0
+let ss = 0.01
+let s0 = 0.995
+let i0 = 0.005
+let r0 = 0.0
+let a = 2.0
+let b = 1.0
+let dr = 0.01
 
 function setCityData(city) {
     if (city === "sf") {
-        s0 = 874961.0;
+        pop = 874961.0
     }
     if (city === "nyc") {
-        s0 = 18804000.0;
-        i0 = 1000.0;
+        pop = 18804000.0
     }
     if (city === "boston") {
-        s0 = 4309000.0;
-        i0 = 3000.0;
-        ss = 0.001;
-        console.log(s0);
-        console.log(i0);
+        pop = 4309000.0
     }
 }
 
 function setVirusData(virus) {
     if (virus === "covid") {
-        a = 2.0;
-        b = 1.0;
+        a = 2.0
+        b = 1.0
+        dr = 0.01
     }
     if (virus === "flu") {
-        a = 3.0;
-        b = 2.5;
+        a = 3.0
+        b = 2.5
+        dr = 0.05
     }
     if (virus === "cold") {
-        a = 1.5;
-        b = 2.8;
+        a = 1.5
+        b = 2.8
+        dr = 0.001
     }
 }
 
 function setTimeLength(time) {
-    t = time;
-    console.log(t);
+    t = time
+    console.log(t)
+}
+
+function setInitialInfectedData(infected) {
+    i0 = infected / pop
+    s0 = 1 - i0
+    console.log(i0)
+    console.log(s0)
 }
 
 
 function dS(a,S,I){
-    return -a*S*I;
+    return -a*S*I
 }
 function dI(a,b,S,I){
-    return a*S*I -b*I;
+    return a*S*I -b*I
 }
 function dR(b,I){
-    return b*I;
+    return b*I
 }
 function SIREulers(){
-    let S = s0;
-    let I = i0;
-    let R = r0;
-    //var cstep = t0;
+    let S = s0
+    let I = i0
+    let R = r0
+    //var cstep = t0
     for(let i = 0; i < ((t - t0) / ss); i++){
-        let s = S+ss*dS(a,S,I);
-        let i = I+ss*dI(a,b,S,I);
-        let r = R+ss*dR(b,I);
-        S=s>0?s:0;
-        I=i>0?i:0;
-        R=r>0?r:0;
-        //cstep++;
+        let s = S+ss*dS(a,S,I)
+        let i = I+ss*dI(a,b,S,I)
+        let r = R+ss*dR(b,I)
+        S = s>0?s:0
+        I = i>0?i:0
+        R = r>0?r:0
+        //cstep++
     }
-    return [S,I,R];
+    S = S * pop
+    I = I * pop
+    R = R * pop
+    let SS = Math.round(S)
+    let CI = Math.round(I)
+    let TI = Math.round(I + R)
+    let Rec = Math.round(R * (1 - dr))
+    let D = Math.round(R * dr)
+
+    return [SS, CI, TI, Rec, D]
 }
