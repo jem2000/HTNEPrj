d46a3ddf3d63ff0f88754282eddfe73cc82f8d97
Justin Melville <56002757+jem2000@users.noreply.github.com>
2021-01-16 04:17:33 -0500

    Update SIR_Form.js

--- SIR_Form.js
+++ SIR_Form.js
@@ -8,31 +8,50 @@
 // b is the rate of recovery
 
 //global vars
-let t = 0;
-let t0 = 0;
-let ss;
-let s0 = 874961;
-let i0 = 0;
-let r0 = 0;
-let a = 2.2;
-let b = 3.0;
+let t = 20;
+let t0 = 0.0;
+let ss = 0.1;
+let s0 = 0.995;
+let i0 = 0.005;
+let r0 = 0.0;
+let a = 2.0;
+let b = 1.0;
 
 function setCityData(city) {
-    if (city === "nyc")
-        s0 = 18804000;
-    if (city === "boston")
-        s0 = 4309000;
+    if (city === "sf") {
+        s0 = 874961.0;
+    }
+    if (city === "nyc") {
+        s0 = 18804000.0;
+        i0 = 1000.0;
+    }
+    if (city === "boston") {
+        s0 = 4309000.0;
+        i0 = 3000.0;
+        ss = 0.001;
+        console.log(s0);
+        console.log(i0);
+    }
 }
 
 function setVirusData(virus) {
+    if (virus === "covid") {
+        a = 2.0;
+        b = 1.0;
+    }
     if (virus === "flu") {
-        a = 3;
+        a = 3.0;
         b = 2.5;
     }
     if (virus === "cold") {
         a = 1.5;
         b = 2.8;
     }
+}
+
+function setTimeLength(time) {
+    t = time;
+    console.log(t);
 }
 
 
@@ -50,13 +69,13 @@ function SIREulers(){
     let I = i0;
     let R = r0;
     //var cstep = t0;
-    for(let i=0;i< ((t-t0)/ss);i++){
+    for(let i = 0; i < ((t - t0) / ss); i++){
         let s = S+ss*dS(a,S,I);
         let i = I+ss*dI(a,b,S,I);
         let r = R+ss*dR(b,I);
-        S=s;
-        I=i;
-        R=r;
+        S=s>0?s:0;
+        I=i>0?i:0;
+        R=r>0?r:0;
         //cstep++;
     }
     return [S,I,R];
