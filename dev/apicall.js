db4e81404916766208d05176d14436c90228c062
George Kent-Scheller <georgeks@bu.edu>
2021-01-16 15:26:35 -0500

    added a jason lookup

--- dev/apicall.js
+++ dev/apicall.js
@@ -1,18 +1,28 @@
-var stateData; 
-function getSIRfromData(t,state){// gets covid data from a certain state based on the day time converted to a string  state in state code abrevs
+
+async function getSIRfromData(t,state){// gets covid data from a certain state based on the day time converted to a string  state in state code abrevs
 	var sp1 = "https://api.covidtracking.com/v1/states/";
 	var sp3 = "/daily.json";
 	var fullstring = sp1+String(state).toLowerCase()+sp3;
-	querryapi(fullstring);
-	//console.log(stateData[0].date);
-	var S;
-	var I; 
-	var R;
+	var index = t;
+	const data = await getData(fullstring);
+	console.log({ data });
+
+	console.log(data[t].date);
+	console.log(data[t].total-data[t].positive-data[t].death);
+	console.log(data[t].positive);
+	console.log(data[t].death);
+	var S = await data[t].date;
+	var I = await data[t].positive;; 
+	var R = await data[t].death + 43;
 	return [S,I,R];
 }
-function querryapi(instring){
-   fetch(instring)
-  .then(response => response.json())
-  .then(json => console.log(json));
+async function getData(url) {
+  const response = await fetch(url);
+  return response.json();
 }
+// async function querryapi(instring){
+	
+//   	return { data };
+// }
+
 getSIRfromData(0,"ca");
