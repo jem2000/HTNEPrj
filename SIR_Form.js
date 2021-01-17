ba050f021202a200479d4d8fc1b1de7d2810c057
Justin Melville <56002757+jem2000@users.noreply.github.com>
2021-01-16 22:38:30 -0500

    Update SIR_Form.js

--- SIR_Form.js
+++ SIR_Form.js
@@ -50,7 +50,7 @@ function setVirusData(virus) {
 }
 
 function setTimeLength(time) {
-    t = time
+    t = time / 7
     console.log(t)
 }
 
@@ -112,7 +112,7 @@ function DrawCircles(S,I,R){
 
     centerX = c.width/2;
     centerY = c.height/2;
-    ctx.globalAlpha = 0.95;
+    ctx.globalAlpha = 0.70;
     ctx.fillStyle = "red";
     ctx.beginPath();
     ctx.arc(centerX, centerY, shortervar*sizechangeI, 0, 2 * Math.PI);
@@ -141,3 +141,90 @@ function DrawCircles(S,I,R){
 
 }
 
+function setValues() {
+    let SS = document.getElementById('SS')
+    let CI = document.getElementById('CI')
+    let TI = document.getElementById('TI')
+    let R = document.getElementById('R')
+    let D = document.getElementById('D')
+
+    let array = SIREulers()
+
+    SS.value = array[0]
+    CI.value = array[1]
+    TI.value = array[2]
+    R.value = array[3]
+    D.value = array[4]
+}
+
+
+
+
+
+async function getSIRfromData(t,state){// gets covid data from a certain state based on the day time converted to a string  state in state code abrevs
+    let sp1 = "https://api.covidtracking.com/v1/states/";
+    let sp3 = "/daily.json";
+    let fullstring = sp1+String(state).toLowerCase()+sp3;
+
+    const data = await getData(fullstring);
+
+    let index = (data.length -1) - t;
+    index = (index<0)?0:index;
+
+    console.log("index=",index);
+
+    let S = await data[index].total-data[index].positive-data[index].death;
+    let I = await data[index].positive;
+    let D = await data[index].death ;
+
+    console.log("date=",data[index].date);
+    console.log("Susceptible=",data[index].total-data[index].positive-data[index].death);
+    console.log("Infected=",data[index].positive);
+    console.log("recovered=",data[index].death);
+    console.log(typeof data[index].death);
+    console.log(data.length);
+
+
+    return [S, I, D];
+}
+
+
+
+async function getData(url) {
+    const response = await fetch(url);
+    return response.json();
+}
+
+const D = document.getElementById('D');
+const asyncButton = document.getElementById('asyncButton');
+
+asyncButton.addEventListener('click', async () => {
+    console.log('clicked')
+    try {
+        var array = (await getSIRfromData(52, 'ca'));
+        D.value = array[0];
+
+
+    } catch(error) {
+        D.value = `There was a problem: ${error}`;
+    }
+})
+
+
+function setAPIValues() {
+    let API_SS = document.getElementById('API_SS')
+    let API_CI = document.getElementById('API_CI')
+    let API_D = document.getElementById('API_D')
+    
+    let array;
+    if (pop === 874961.0)
+        array = getSIRfromData(t, 'ca')
+    if (pop === 18804000.0) 
+        array = getSIRfromData(t, 'ny')
+    if (pop === 4309000.0)
+        array = getSIRfromData(t, 'ma')
+    
+    API_SS.value = array[0]
+    API_CI.value = array[1]
+    API_D.value = array[2]
+}
